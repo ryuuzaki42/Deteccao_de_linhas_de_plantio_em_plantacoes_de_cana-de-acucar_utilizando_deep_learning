27fea1abe93a02397faf7b9778c82ba4f3e04a48
João Batista <joao42lbatista@gmail.com>
2024-06-13 15:51:14 -0300

    add see_pixel_values.r

---
+++ outros/see_pixel_values.r
@@ -0,0 +1,14 @@
+
+## See pixel values
+    Step 1: Load the image
+
+    ## IrfanView
+        Click on any part of the image and hold
+            Pixel value will show in the windows title
+
+    ## Gimp
+        In the top right conner, in the arrow to left
+            Click and go to Add Tab > Pointer
+
+    ## Geeqie
+        View > Pixel Info
